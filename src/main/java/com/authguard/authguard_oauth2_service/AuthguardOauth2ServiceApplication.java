b84c984d422d2a4b70111956ced0e360c220fd82
Ruddarm <ruddarmmaurya@gmail.com>
2025-08-06 18:27:13 +0530

    initial setup for oauth2 servie

---
+++ src/main/java/com/authguard/authguard_oauth2_service/AuthguardOauth2ServiceApplication.java
@@ -0,0 +1,13 @@
+package com.authguard.authguard_oauth2_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthguardOauth2ServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthguardOauth2ServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/authguard/authguard_oauth2_service/AuthguardOauth2ServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.authguard.authguard_oauth2_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthguardOauth2ServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
